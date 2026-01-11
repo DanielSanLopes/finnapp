aa2cd37c4c245d5d78fecb0941cd21745d46ab4a
DanielSanLopes <34308537+DanielSanLopes@users.noreply.github.com>
2026-01-11 08:01:03 +0000

    adding `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function statusHandler(req, res) {
+import db from "../../../../infra/db.js";
+
+async function statusHandler(req, res) {
+  const result = await db.query("SELECT NOW() AS current_time;");
+  console.log(result.rows);
   res.status(200).send({ message: "Deu certo, mãe!" });
 }
 
