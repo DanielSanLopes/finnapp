fc476ec822823c89a53d030e73353872d84eda87
DanielSanLopes <34308537+DanielSanLopes@users.noreply.github.com>
2026-02-01 02:09:32 +0000

    prevent SQL Injection and finish `status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,49 @@
 import db from "infra/db.js";
 
 async function statusHandler(req, res) {
-  const result = await db.query("SELECT NOW() AS current_time;");
-  console.log(result.rows);
-  res.status(200).send({ message: "Deu certo, mãe!" });
+  //-------------------------------------------------------------------------------------------
+  // const result = await db.query(
+  //   `SELECT 'Version' as Informacao, VERSION() as Valor
+  //     UNION ALL
+  //     SELECT 'Max Connections', setting FROM pg_settings WHERE name = 'max_connections';`,
+  // );
+  //-------------------------------------------------------------------------------------------
+
+  //#region Uncomment to enable more detailed checks
+  //const result = await db.query(`SELECT VERSION() as version, current_setting('max_connections') as max_connections,
+  // (select count(*) from pg_stat_activity where backend_type = 'client backend') as active_connections;`);
+  //#endregion
+
+  const databaseVersionResult = await db.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await db.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB || "postgres";
+  console.log("Selected Database: ", databaseName);
+  const databaseActiveConnectionsResult = await db.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseActiveConnectionsValue =
+    databaseActiveConnectionsResult.rows[0].count;
+
+  console.log("Active Connections:", databaseActiveConnectionsValue);
+  // res.status(200).send({ message: "Deu certo, mãe!" });
+  const updatedAt = new Date().toISOString();
+  // console.log("result:", { updated_at: updatedAt, ...result.rows[0] });
+  res.status(200).send({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        active_connections: databaseActiveConnectionsValue,
+      },
+    },
+  });
 }
 
 export default statusHandler;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,43 @@
 test("get/api/v1/status should return 200", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
   expect(res.status).toBe(200);
+
+  const responseBody = await res.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  // console.log(
+  //   "Response Body: ",
+  //   JSON.stringify(responseBody, null, 2).toString(),
+  // );
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parsedUpdatedAt).not.toBe(new Date(null).toISOString());
+  // expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  // #region Uncomment to enable more detailed checks
+  // expect(responseBody.version).toBeDefined();
+  // expect(responseBody.max_connections).toBeDefined();
+  // expect(responseBody.active_connections).toBeDefined();
+
+  // expect(typeof responseBody.version).toBe("string");
+  // expect(typeof responseBody.max_connections).toBe("string");
+  // expect(typeof responseBody.active_connections).toBe("string");
+
+  // expect(responseBody.version).not.toBe("");
+  // expect(responseBody.max_connections).not.toBe("");
+  // expect(responseBody.active_connections).not.toBe("");
+  // #endregion
+
+  expect(responseBody.dependencies.database.version).toEqual("16.11");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.active_connections).toEqual(1);
+
+  console.log(responseBody);
 });
+
+// test.only("Teste de SQL Injection", async () => {
+//   // await fetch("http://localhost:3000/api/v1/status?databaseName=local_db");
+//   await fetch(
+//     "http://localhost:3000/api/v1/status?databaseName=local_db'; SELECT pg_sleep(4); --",
+//   );
+// });
